99f43afe6b4dee59bbccd078462953edf275e56e
pranalikubade <pranalikubade2000@gmail.com>
2023-01-05 18:09:14 +0530

    added: uc2_CheckWinorLoose

--- src/com/bridgrlabz/GamlingSimulationProblem.java
+++ src/com/bridgrlabz/GamlingSimulationProblem.java
@@ -1,10 +1,24 @@
 package com.bridgrlabz;
 
 public class GamlingSimulationProblem {
-    static  final int STAKE_PER_DAY=100;
-    static  final int BET_PER_DAY=1;
+    static int STAKE_PER_DAY = 100;
+    static final int BET_PER_GAME = 1;
+
+    static void CheckWinOrLoose() {
+        int random = (int) (Math.random() * 2);
+        if (random == 0) {
+            System.out.println("Gambler won the Bet");
+            STAKE_PER_DAY = STAKE_PER_DAY + BET_PER_GAME;
+            System.out.println("Gambler Account Balance: " + STAKE_PER_DAY);
+        } else {
+            System.out.println("Gambler loose the Bet");
+            STAKE_PER_DAY = STAKE_PER_DAY - BET_PER_GAME;
+            System.out.println("Gambler Account Balance: " + STAKE_PER_DAY);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gamling Simulation Problem");
+        CheckWinOrLoose();
     }
 }
