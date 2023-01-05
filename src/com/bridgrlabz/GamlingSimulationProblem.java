6b7adcd8df13a852b4180efd8a5222a10a83a171
pranalikubade <pranalikubade2000@gmail.com>
2023-01-05 16:55:01 +0530

    added: Gamling Simulation Problem

---
+++ src/com/bridgrlabz/GamlingSimulationProblem.java
@@ -0,0 +1,10 @@
+package com.bridgrlabz;
+
+public class GamlingSimulationProblem {
+    static  final int STAKE_PER_DAY=100;
+    static  final int BET_PER_DAY=1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gamling Simulation Problem");
+    }
+}
